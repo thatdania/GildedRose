77e58dbe3eb64bf160488cfc823553241e87faef
thatdania@gmail.com <thatdania@gmail.com>
2018-01-04 11:17:50 +0000

    Edit ReadMe
    for focused on my approach and track what needs to be required

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,21 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+
+  beforeEach( function(){
+    item = new Item("food",5,5);
+    gildedRose = new Shop([item]);
   });
+
+  describe("#notAgeBrie", function(){
+    it("#notAgeBrie should minus 1 from the quality", function(){
+      gildedRose.notAgeBrie(5);
+      expect(gildedRose[0].quality).toEqual("4")
+    });
+  });
+  // it("should foo", function() {
+  //   const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  //   const items = gildedRose.updateQuality();
+  //   expect(items[0].name).toEqual("fixme");
+  // });
 
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -18,26 +18,30 @@ class Shop {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
-      } else {
+      }
+
+      else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
+
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
+
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -46,14 +50,18 @@ class Shop {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
-          } else {
+          }
+          else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
-        } else {
+        }
+
+        else {
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
+
       }
     }
 
