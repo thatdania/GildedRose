13e4f51d8661c7818c6f6dbbd27c24285ef80acf
thatdania@gmail.com <thatdania@gmail.com>
2018-01-04 11:18:54 +0000

    Add fail test for not age Brie
    Writing a test for the first method for not age Brie.
    Test fails

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -2,14 +2,14 @@ describe("Gilded Rose", function() {
 
 
   beforeEach( function(){
-    item = new Item("food",5,5);
+    item = new Item("food",4,4);
     gildedRose = new Shop([item]);
   });
 
   describe("#notAgeBrie", function(){
     it("#notAgeBrie should minus 1 from the quality", function(){
-      gildedRose.notAgeBrie(5);
-      expect(gildedRose[0].quality).toEqual("4")
+      gildedRose.notAgeBrie(4);
+      expect(gildedRose[0].quality).toEqual("3")
     });
   });
   // it("should foo", function() {
